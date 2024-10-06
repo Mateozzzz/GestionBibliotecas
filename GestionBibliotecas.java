272f877bc5219a273990793aae94f42f4e7c181a
Mateo <109302760+Mateozzzz@users.noreply.github.com>
2024-10-06 17:31:28 -0500

    First Update
    The Project isn't finish, i need better Data Structures, Flow Control and the quality code.

--- GestionBibliotecas.java
+++ GestionBibliotecas.java
@@ -1,9 +1,10 @@
 package gestionbibliotecas;
 
 import java.util.ArrayList;
-//import java.util.Arrays;
-
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class GestionBibliotecas {
 
@@ -12,88 +13,150 @@ public class GestionBibliotecas {
 //Declaration Variables
 Scanner entrada = new Scanner(System.in);
 
-//ArrayList For Users
-ArrayList<String> createUsers = new ArrayList<>();
-ArrayList<String> createBooks = new ArrayList<>();
-
-       
-        
-        
-    // User Input
-    //System.out.println("[ + ] Presiona [1] para Iniciar...");
-    int userEntrance = 7;
+//Data Structures Used
+//ArrayList<String> createUsers = new ArrayList<String>();
+ArrayList<String> createBooks = new ArrayList<String>();
+Stack<String> requestBooks = new Stack<>();
+Queue<String> turnBooks = new LinkedList<>();
+LinkedList<String> createUsers = new LinkedList<String>();
+   
+// User Input
+int userEntrance = 7;
      
-    do{   
-        //Frontend
-        System.out.println("+----------------------------+");
-        System.out.println("|   BOOKS MANAGEMENT STORE   |");
-        System.out.println("+----------------------------+");
-        System.out.println("       Mateo Henao Correa    ");
-        System.out.println("         by @Mateozzzz        ");
-        System.out.println("[+] [1] Creacion de Usuario");
-        System.out.println("[+] [2] Ingreso Libros");
-        System.out.println("[+] [3] Mostrar Todos Libros");
-        System.out.println("[+] [4] Mostar Todos Usuario");
-        System.out.println("[+] [5] Peticion Prestamos");
-        System.out.println("[+] [6] Devolucion Libros");
-        System.out.println("[+] [7] Salir                  ");
-        System.out.println("                              ");
-        System.out.println("Ingrese su Opcion ==> ");
-        System.out.println("                              ");
-        
-
-//entrada.nextLine();
-
-    //User Option
-     int userOption = entrada.nextInt();
+do{   
+    //Frontend
+    System.out.println("+----------------------------+");
+    System.out.println("|   BOOKS MANAGEMENT STORE   |");
+    System.out.println("+----------------------------+");
+    System.out.println("       Mateo Henao Correa    ");
+    System.out.println("         by @Mateozzzz        ");
+    System.out.println("[+] [1] Creacion de Usuario");
+    System.out.println("[+] [2] Ingreso Libros");
+    System.out.println("[+] [3] Mostrar Todos Libros");
+    System.out.println("[+] [4] Mostar Todos Usuario");
+    System.out.println("[+] [5] Peticion Prestamos");
+    System.out.println("[+] [6] Devolucion Libros");
+    System.out.println("[+] [7] Salir                  ");
+    System.out.println("                               ");
+    System.out.printf ("Ingrese su Opcion ==> ");
     
-     switch(userOption){
-        case 1:
-            entrada.nextLine();
-            System.out.println("Ingrese su Cedula ==> ");
-            String idUser = entrada.nextLine();
-            createUsers.add(idUser);
-            System.out.println("Ingrese Su Nombre ==> ");
-            String nameUser = entrada.nextLine();
-            createUsers.add(nameUser);
-            System.out.println("El Usuario "+nameUser+" con cedula "+idUser+
-            " Ha sido creado Exitosamente ");
-            //Equals
-            
-            break;
-        case 2:
-            entrada.nextLine();
-            System.out.println("Ingrese Codigo Libro ==> ");
-            String bookIsb = entrada.nextLine();
-            createBooks.add(bookIsb);
-            System.out.println("Ingrese Titulo Libro ==> ");
-            String bookTitule = entrada.nextLine();
-            createBooks.add(bookTitule);
-            System.out.println("El Libro "+bookIsb+" con Codigo "+bookTitule+
-            " Ha sido agregado");
-  
-            break;
-        case 3:
-            for (int i =0; i<createBooks.size(); i++){
-                System.out.println(createBooks.get(i));}
-            break;
-        case 4:
-            for (int i =0; i<createUsers.size(); i++){
-                System.out.println(createUsers.get(i));}
-        case 5:
-            System.out.println("5");
-            break;
-        case 6:
-            System.out.println("6");
-        case 7:
-            System.out.println("7");
-            break;
-        default:System.out.println("Opcion Inavalida,Escoga Nuevamente.");
-            break;
-     }  
 
-        }while(userEntrance != 0);
+//User Option and Switch Cases
+ int userOption = entrada.nextInt();
 
- }
+ switch(userOption){
+    case 1:
+        if(createUsers.isEmpty()){
+            System.out.println("No hay usuarios registrados");}
+        // Add Users
+        entrada.nextLine();
+        System.out.printf("Ingrese su Cedula ==> ");
+        String idUser = entrada.nextLine();
+        if(createUsers.contains(idUser)){ 
+            System.out.println("! Usuario regitrado en Sistema !");          
+            break;}
+        else{createUsers.add(0,idUser);}
+        // Duplicate Users
+        System.out.printf("Ingrese Su Nombre ==> ");              
+        String nameUser = entrada.nextLine();
+        if(createUsers.contains(nameUser)){         
+            System.out.println("! Nombre Registrado en Sistema !");            
+            break;}else{createUsers.add(1,nameUser);}
+        // Show Users
+        System.out.println("Usuario creado Exitosamente!! "+createUsers);
+        break;
+    case 2:
+        entrada.nextLine();
+        //Add New Book
+        System.out.printf("Ingrese Codigo Libro ==> ");
+        String bookIsb = entrada.nextLine();
+        if(createBooks.contains(bookIsb)){
+            System.out.printf("! El libro ya esta en el Sistema !");
+            break;}
+        else{createBooks.add(bookIsb);}
+        // Duplicate Books
+        System.out.printf("Ingrese el titulo de su libro ==> ");              
+        String bookTitule = entrada.nextLine();
+        if(createBooks.contains(bookTitule)){
+            System.out.printf("! Titulo Invalido !");                  
+            break;}
+        else{createBooks.add(bookTitule);}
+        //Show Books   
+        System.out.println("Libro Agregado Exitosamente ! "+createBooks);
+        break;
+    case 3:
+        if(createBooks.isEmpty()){
+            System.out.println("! No hay libros en la estanteria !");}
+        // Add Books
+        System.out.printf("| %-5s | %-5s |%n","Codigo","Titulo");
+        System.out.println(createBooks);
+        /*
+        Add Delete Users Feature
+        */           
+        break;
+    case 4:
+        if(createUsers.isEmpty()){
+            System.out.println("! No hay Usuarios registrados !");}
+        // Show Users
+        System.out.printf("| %-5s | %-5s |%n","Cedula","Nombre");
+        System.out.println(createUsers);
+        /*
+        Add Delete Book Feature
+        */
+        break;
+    case 5:
+        if(requestBooks.empty()){
+            System.out.println("No se encuentran Peticiones registradas");}        
+        // Add Request
+        entrada.nextLine();
+        System.out.printf("Ingresa tu Usuario ==> ");
+        String requestUser = entrada.nextLine();
+        System.out.printf("Ingresa el Codigo del Libro ==> ");
+        String requestBook = entrada.nextLine();
+        
+        if(createUsers.contains(requestUser)&&
+                createBooks.contains(requestBook)){                             
+        // Delete from Books
+   
+        createBooks.remove(requestBook);
+        createBooks.add(0, "En Prestamo");
+        //createBooks.remove(requestBook);
+        // Add to ReuqestBooks
+        requestBooks.push(requestUser+requestBook);
+        //requestBooks.push(requestBook);
+        System.out.println("El Prestamo ha sido cargado ");
+        System.out.print(requestBooks.peek());}
+        // Repeat Book is not there and ww nee quit title of book
+        else{System.out.println("! Usuario o Libro no encontrados !");}
+        break;  
+    case 6:
+        if(turnBooks.isEmpty()){
+            System.out.println("Actualmente no se tiene Devoluciones");
+            // Add Return Book
+            entrada.nextLine();
+            System.out.printf("Ingrese codigo del libro a devolver ==> ");
+            String loadBook = entrada.nextLine();
+            System.out.printf("Ingrese usuario de la devolucion ==> ");
+            String loadUser = entrada.nextLine();
+            // Duplicate Books
+            if(requestBooks.contains(loadBook)&&
+                    requestBooks.contains(loadUser)){
+            // Remove From request
+            requestBooks.remove(loadBook);
+            requestBooks.remove(loadUser);
+            //add to Array
+            createBooks.set(0,loadBook);
+            System.out.println("El Libro ha sido añadido exitosamente");
+            System.out.println(turnBooks.peek());
+            }else{System.out.println("Usuario o Libro no encontrados");} 
+        }
+        break;
+    case 7:
+        break;
+    default:System.out.println("Opcion Invalida, Escoga Nuevamente.");
+ }  
+    }while(userEntrance != 0);
+
+}
     
 }   
